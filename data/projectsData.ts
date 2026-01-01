0b789726425dc94c0b496606b251829fb72f4b54
Jeremy Garrell <garrellj101@gmail.com>
2026-01-01 13:39:22 -0500

    Add project reference for open source dialer

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,6 +7,12 @@ interface Project {
 
 const projectsData: Project[] = [
   {
+    title: 'Open Source Predictive Dialer',
+    description: `Join my journey learning predictive dialers by building one`,
+    imgSrc: '/static/images/gts-dialer.png',
+    href: 'https://github.com/garrell-tech-solutions/dialer',
+  },
+  {
     title: 'Callpurity Dashboard',
     description: `Unlock the power of your outbound calling strategy with the Callpurity Dashboard – a robust platform designed to transform raw data into actionable insights.`,
     imgSrc: '/static/images/CP_FullLogo_Standard.png',
@@ -17,7 +23,7 @@ const projectsData: Project[] = [
     description: `Workflow aid used to audit call center agent paystubs against health insurance client lists`,
     imgSrc: '/static/images/aca-tracker-logo.png',
     href: 'https://aca-tracker.fly.dev/',
-  }
+  },
 ]
 
 export default projectsData
